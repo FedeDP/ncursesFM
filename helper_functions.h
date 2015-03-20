06067deaf0dfefba9205f0455b7420903e2321d7
Federico Di Pierro <nierro92@gmail.com>
2015-03-20 15:11:44 +0100

    removed old files

--- helper_functions.h
+++
@@ -1,6 +0,0 @@
-#include "ui_functions.h"
-
-int isIso(char *ext);
-int file_isCopied(void);
-void get_full_path(char *full_path_current_position);
-int ask_user(char *str);
--- quit_functions.h
+++
@@ -1,6 +0,0 @@
-#include <pthread.h>
-#include "helper_functions.h"
-
-void free_copied_list(copied_file_list *h);
-void free_everything(void);
-void quit_func(void);
