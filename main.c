177482b81d88dd4266a87e4069dccb3dabb2a95c
Federico Di Pierro <nierro92@gmail.com>
2015-03-20 15:04:43 +0100

    added licensing in every .c file

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
  *
  * Copyright (C) 2015  Federico Di Pierro <nierro92@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of ncursesFM.
+ * ncursesFM is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
--- quit_functions.c
+++ quit_functions.c
@@ -1,3 +1,26 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ *
+ * NcursesFM: file manager in C with ncurses UI for linux.
+ * https://github.com/FedeDP/ncursesFM
+ *
+ * Copyright (C) 2015  Federico Di Pierro <nierro92@gmail.com>
+ *
+ * This file is part of ncursesFM.
+ * ncursesFM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
 #include "quit_functions.h"
 
 void free_copied_list(copied_file_list *h)
