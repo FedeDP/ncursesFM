FedeDP/ncursesFM

commit f125053d70b05b052e8d4f607f8d517ba6274e20

feature added: highlight of selected files

inc/ui_functions.h
@@ -39,5 +39,6 @@ void print_info(const char *str, int i);
 void ask_user(const char *str, char *input, int d, char c);
 void resize_win(void);
 void change_sort(void);
+void highlight_selected(int line, const char c);
 
 #endif

src/ui_functions.c
@@ -16,6 +16,7 @@ static void change_unit(float size, char *str);
 static void erase_stat(void);
 static void resize_helper_win(void);
 static void resize_fm_win(void);
+static void check_selected(const char *str, int win, int line);
 
 struct scrstr {
     WINDOW *fm;
@@ -158,18 +159,25 @@ void reset_win(int win)
  * If stat_active == STATS_ON for 'win', and 'win' is not in search mode, it prints stats about size and permissions for every file.
  */
 void list_everything(int win, int old_dim, int end) {
+    char *str;
+    int width;
+    
     pthread_mutex_lock(&fm_lock);
     if (end == 0) {
         end = dim - 2;
     }
     wattron(mywin[win].fm, A_BOLD);
     for (int i = old_dim; (i < ps[win].number_of_files) && (i  < old_dim + end); i++) {
-        colored_folders(win, *(str_ptr[win] + i));
         if ((sv.searching == 3 + win) || (device_mode == 1 + win)) {
-            mvwprintw(mywin[win].fm, 1 + i - mywin[win].delta, 4, "%.*s", mywin[win].width - 5, *(str_ptr[win] + i));
+            width = mywin[win].width - 5;
+            str = *(str_ptr[win] + 1);
         } else {
-            mvwprintw(mywin[win].fm, 1 + i - mywin[win].delta, 4, "%.*s", MAX_FILENAME_LENGTH, strrchr(*(str_ptr[win] + i), '/') + 1);
+            check_selected(*(str_ptr[win] + i), win, i);
+            width = MAX_FILENAME_LENGTH;
+            str = strrchr(*(str_ptr[win] + i), '/') + 1;
         }
+        colored_folders(win, *(str_ptr[win] + i));
+        mvwprintw(mywin[win].fm, 1 + i - mywin[win].delta, 4, "%.*s", width, str);
         wattroff(mywin[win].fm, COLOR_PAIR);
     }
     wattroff(mywin[win].fm, A_BOLD);
@@ -634,5 +642,35 @@ void change_sort(void) {
     }
     for (int i = 0; i < cont; i++) {
         tab_refresh(i);
+    }
+}
+
+/*
+ * Called in manage_space_press() (fm_functions.c)
+ * It checks for each fm win, if they're on the same cwd, then checks
+ * if the file to be highlighted is visible inside "win" -> useful only if win is not the active one.
+ * Then prints c char before current filename and refreshes win.
+ */
+void highlight_selected(int line, const char c) {
+    for (int i = 0; i < cont; i++) {
+        if ((i == active) || ((strcmp(ps[i].my_cwd, ps[active].my_cwd) == 0) 
+            && (line - mywin[i].delta > 0) && (line - mywin[i].delta < dim - 2))) {
+            wattron(mywin[i].fm, A_BOLD);
+            mvwprintw(mywin[i].fm, 1 + line - mywin[i].delta, 3, "%c", c);
+            wattroff(mywin[i].fm, A_BOLD);
+            wrefresh(mywin[i].fm);
+        }
+    }
+}
+
+static void check_selected(const char *str, int win, int line) {
+    file_list *tmp = selected;
+    
+    while (tmp) {
+        if (strcmp(tmp->name, str) == 0) {
+            mvwprintw(mywin[win].fm, 1 + line - mywin[win].delta, 3, "*");
+            break;
+        }
+        tmp = tmp->next;
     }
 }

TODO
@@ -11,6 +11,4 @@
  * debug support: -DENABLE_DEBUG -> somewhere like /tmp/ncursesfm.log that will not survive a reboot.
  *
  * UPDATE README and PKGBUILD with new optdep/function (openssl/shasum support)
- * 
- * FEATURE: add a "*" before selected files in list_everything
  */

src/fm_functions.c
@@ -216,19 +216,23 @@ int remove_file(void) {
 }
 
 /*
- * If there are no selected files, or the file user selected wasn't already selected (call to remove_from_list() ),
+ * If there are no selected files, or the file user selected wasn't already selected (remove_from_list() returns 0),
  * add this file to select list.
  */
 void manage_space_press(const char *str) {
     const char *s;
+    char c;
 
     if ((!selected) || (remove_from_list(str) == 0)) {
         selected = select_file(selected, str);
         s = file_sel[0];
+        c = '*';
     } else {
+        c = ' ';
         ((selected) ? (s = file_sel[1]) : (s = file_sel[2]));
     }
     print_info(s, INFO_LINE);
+    highlight_selected(ps[active].curr_pos, c);
 }
 
 /*

commit 09b24b1f3d43816e2d61525747b6629916d3869e

Fixed some ui small bugs. Safer: devices_tab and list_found will lock fm_lock too. Fixed a bug: user could open 2 devices_tab. Limit should be one (2 are useless). Fixed a bug introduced in f125053d70b05b052e8d4f607f8d517ba6274e20 in list_everything: a '*(str_ptr[win] + i)' became '*(str_ptr[win] + 1)'

inc/declarations.h
@@ -33,6 +33,12 @@
 #define NORM_QUIT 1
 #define MEM_ERR_QUIT 2
 
+/*
+ * Just a flag to be passed to list_found_or_devices() in ui_functions.c
+ */
+#define SEARCH 0
+#define DEVICE 1
+
 struct conf {
     char editor[PATH_MAX];
     int show_hidden;
@@ -89,3 +95,4 @@ int device_mode;
 #endif
 #endif
 char (*str_ptr[MAX_TABS])[PATH_MAX]; // pointer to make abstract which list of strings i have to print in list_everything()
+pthread_mutex_t fm_lock;

inc/ui_functions.h
@@ -22,10 +22,6 @@
 
 void screen_init(void);
 void screen_end(void);
-int sizesort(const struct dirent **d1, const struct dirent **d2);
-int last_mod_sort(const struct dirent **d1, const struct dirent **d2);
-void reset_win(int win);
-void list_everything(int win, int old_dim, int end);
 void new_tab(int win);
 void restrict_first_tab(void);
 void delete_tab(int win);
@@ -36,6 +32,7 @@ void trigger_show_helper_message(void);
 void trigger_stats(void);
 int win_getch(void);
 void tab_refresh(int win);
+void list_found_or_devices(int num, char (*str)[PATH_MAX], int mode);
 void print_info(const char *str, int i);
 void ask_user(const char *str, char *input, int d, char c);
 void resize_win(void);

src/main.c
@@ -274,7 +274,9 @@ static void main_loop(void) {
 #endif
 #ifdef LIBUDEV_PRESENT
         case 'm': // m to mount/unmount fs
-            devices_tab();
+            if (!device_mode) {
+                devices_tab();
+            }
             break;
 #endif
         case ',': // , to enable/disable fast browse mode

src/mount.c
@@ -111,12 +111,7 @@ static void close_bus(sd_bus_error *error, sd_bus_message *mess, sd_bus *bus) {
 void devices_tab(void) {
     enumerate_usb_mass_storage();
     if (device_mode && !quit) {
-        str_ptr[active] = usb_devices;
-        ps[active].number_of_files = device_mode;
-        device_mode = 1 + active;
-        reset_win(active);
-        sprintf(ps[active].title, device_mode_str);
-        list_everything(active, 0, 0);
+        list_found_or_devices(device_mode, usb_devices, DEVICE);
     } else {
         print_info("No mountable devices found.", INFO_LINE);
     }

src/search.c
@@ -91,19 +91,13 @@ static void *search_thread(void *x) {
         }
     } else {
         sv.searching = 2;
-        print_info("", SEARCH_LINE);
     }
+    print_info("", SEARCH_LINE);
     return NULL;
 }
 
 void list_found(void) {
-    sv.searching = 3 + active;
-    ps[active].number_of_files = sv.found_cont;
-    str_ptr[active] = sv.found_searched;
-    reset_win(active);
-    sprintf(ps[active].title, "Found file searching %s:", sv.searched_string);
-    sprintf(searching_mess[sv.searching - 1], "%d files found.", sv.found_cont);
-    list_everything(active, 0, 0);
+    list_found_or_devices(sv.found_cont, sv.found_searched, SEARCH);
     print_info("", SEARCH_LINE);
 }
 

src/ui_functions.c
@@ -3,6 +3,10 @@
 static void fm_scr_init(void);
 static void info_win_init(void);
 static void generate_list(int win);
+static int sizesort(const struct dirent **d1, const struct dirent **d2);
+static int last_mod_sort(const struct dirent **d1, const struct dirent **d2);
+static void reset_win(int win);
+static void list_everything(int win, int old_dim, int end);
 static void print_border_and_title(int win);
 static int is_hidden(const struct dirent *current_file);
 static void initialize_tab_cwd(int win);
@@ -29,7 +33,7 @@ struct scrstr {
 static struct scrstr mywin[MAX_TABS];
 static WINDOW *helper_win, *info_win;
 static int dim;
-static pthread_mutex_t fm_lock, info_lock;
+static pthread_mutex_t info_lock;
 static int (*sorting_func)(const struct dirent **d1, const struct dirent **d2) = alphasort; // file list sorting function, defaults to alphasort
 
 /*
@@ -124,11 +128,10 @@ static void generate_list(int win) {
     free(files);
     if (!quit) {
         reset_win(win);
-        list_everything(win, 0, 0);
     }
 }
 
-int sizesort(const struct dirent **d1, const struct dirent **d2) {
+static int sizesort(const struct dirent **d1, const struct dirent **d2) {
     struct stat stat1, stat2;
     float result;
     
@@ -138,7 +141,7 @@ int sizesort(const struct dirent **d1, const struct dirent **d2) {
     return (result > 0) ? -1 : 1;
 }
 
-int last_mod_sort(const struct dirent **d1, const struct dirent **d2) {
+static int last_mod_sort(const struct dirent **d1, const struct dirent **d2) {
     struct stat stat1, stat2;
     
     stat((*d1)->d_name, &stat1);
@@ -146,11 +149,12 @@ int last_mod_sort(const struct dirent **d1, const struct dirent **d2) {
     return (stat2.st_mtime - stat1.st_mtime);
 }
 
-void reset_win(int win)
+static void reset_win(int win)
 {
     wclear(mywin[win].fm);
     mywin[win].delta = 0;
     ps[win].curr_pos = 0;
+    list_everything(win, 0, 0);
 }
 
 /*
@@ -159,7 +163,7 @@ void reset_win(int win)
  * Checks if window 'win' is in search/device mode, and takes care.
  * If stat_active == STATS_ON for 'win', and 'win' is not in search mode, it prints stats about size and permissions for every file.
  */
-void list_everything(int win, int old_dim, int end) {
+static void list_everything(int win, int old_dim, int end) {
     char *str;
     int width;
     
@@ -170,7 +174,7 @@ void list_everything(int win, int old_dim, int end) {
     for (int i = old_dim; (i < ps[win].number_of_files) && (i  < old_dim + end); i++) {
         if ((sv.searching == 3 + win) || (device_mode == 1 + win)) {
             width = mywin[win].width - 5;
-            str = *(str_ptr[win] + 1);
+            str = *(str_ptr[win] + i);
         } else {
             check_selected(*(str_ptr[win] + i), win, i);
             width = MAX_FILENAME_LENGTH;
@@ -575,6 +579,25 @@ void tab_refresh(int win) {
         generate_list(win);
         pthread_mutex_unlock(&fm_lock);
     }
+}
+
+void list_found_or_devices(int num, char (*str)[PATH_MAX], int mode) {
+    pthread_mutex_lock(&fm_lock);
+    ps[active].number_of_files = num;
+    str_ptr[active] = str;
+    if (mode == SEARCH) {
+        sv.searching = 3 + active;
+        sprintf(ps[active].title, "Found file searching %s:", sv.searched_string);
+        sprintf(searching_mess[sv.searching - 1], "%d files found.", num);
+    } else {
+        device_mode = 1 + active;
+        sprintf(ps[active].title, device_mode_str);
+    }
+    if (mywin[active].stat_active) {
+        memset(mywin[active].tot_size, 0, strlen(mywin[active].tot_size));
+    }
+    reset_win(active);
+    pthread_mutex_unlock(&fm_lock);
 }
 
 /*