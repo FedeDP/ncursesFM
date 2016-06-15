FedeDP/ncursesFM

commit 501881197627e70b68107a13ca852ff1008cf15e

switched to strncpy and snprintf for safety

inc/declarations.h
@@ -102,7 +102,7 @@ struct conf {
  * List used to store file names when selecting them
  */
 typedef struct list {
-    char name[PATH_MAX];
+    char name[PATH_MAX + 1];
     struct list *next;
 } file_list;
 
@@ -133,7 +133,7 @@ enum working_mode {normal, fast_browse_, bookmarks_, search_, device_, _preview}
  */
 struct tab {
     int curr_pos;
-    char my_cwd[PATH_MAX];
+    char my_cwd[PATH_MAX + 1];
     char (*nl)[PATH_MAX + 1];
     int number_of_files;
     char title[PATH_MAX + 1];

src/archiver.c
@@ -38,7 +38,7 @@ static void archiver_func(void) {
     char path[PATH_MAX + 1];
 
     while (tmp) {
-        strcpy(path, tmp->name);
+        strncpy(path, tmp->name, PATH_MAX);
         distance_from_root = strlen(dirname(path));
         nftw(tmp->name, recursive_archive, 64, FTW_MOUNT | FTW_PHYS);
         tmp = tmp->next;
@@ -52,7 +52,7 @@ static int recursive_archive(const char *path, const struct stat *sb, int typefl
     int len, fd;
     struct archive_entry *entry = archive_entry_new();
 
-    strcpy(entry_name, path + distance_from_root + 1);
+    strncpy(entry_name, path + distance_from_root + 1, PATH_MAX);
     archive_entry_set_pathname(entry, entry_name);
     archive_entry_copy_stat(entry, sb);
     archive_write_header(archive, entry);
@@ -90,7 +90,7 @@ static int try_extractor(file_list *tmp) {
     archive_read_support_filter_all(a);
     archive_read_support_format_all(a);
     if ((a) && (archive_read_open_filename(a, tmp->name, BUFF_SIZE) == ARCHIVE_OK)) {
-        strcpy(path, tmp->name);
+        strncpy(path, tmp->name, PATH_MAX);
         current_dir = dirname(path);
         extractor_thread(a, current_dir);
         return 0;
@@ -117,20 +117,20 @@ static void extractor_thread(struct archive *a, const char *current_dir) {
     archive_write_disk_set_options(ext, flags);
     archive_write_disk_set_standard_lookup(ext);
     while (archive_read_next_header(a, &entry) != ARCHIVE_EOF) {
-        strcpy(name, archive_entry_pathname(entry));
+        strncpy(name, archive_entry_pathname(entry), PATH_MAX);
         num = 0;
         /* avoid overwriting a file/dir in path if it has the same name of a file being extracted there */
         if (strchr(name, '/')) {
-            strcpy(tmp_name, strchr(name, '/'));
+            strncpy(tmp_name, strchr(name, '/'), PATH_MAX);
         } else {
             tmp_name[0] = '\0';
         }
         len = strlen(name) - strlen(tmp_name);
         while (access(name, F_OK) == 0) {
             num++;
-            sprintf(name + len, "%d%s", num, tmp_name);
+            snprintf(name + len, PATH_MAX - len, "%d%s", num, tmp_name);
         }
-        sprintf(fullpathname, "%s/%s", current_dir, name);
+        snprintf(fullpathname, PATH_MAX, "%s/%s", current_dir, name);
         archive_entry_set_pathname(entry, fullpathname);
         archive_write_header(ext, entry);
         len = archive_read_data(a, buff, sizeof(buff));

src/bookmarks.c
@@ -13,8 +13,8 @@ static char (bookmarks[MAX_BOOKMARKS])[PATH_MAX + 1];
 void get_bookmarks(void) {
     FILE *f;
     
-    strcpy(home_dir, getpwuid(getuid())->pw_dir);
-    sprintf(fullpath, "%s%s", home_dir, bookmarks_file);
+    strncpy(home_dir, getpwuid(getuid())->pw_dir, PATH_MAX);
+    snprintf(fullpath, PATH_MAX, "%s%s", home_dir, bookmarks_file);
     get_xdg_dirs();
     if (num_bookmarks < MAX_BOOKMARKS) {
         if ((f = fopen(fullpath, "r"))) {
@@ -40,15 +40,15 @@ static void get_xdg_dirs(void) {
     char line[1000], file_path[PATH_MAX + 1];
     const char path[] = "/.config/user-dirs.dirs";
 
-    sprintf(file_path, "%s%s", home_dir, path);
+    snprintf(file_path, PATH_MAX, "%s%s", home_dir, path);
     if ((f = fopen(file_path, "r"))) {
         while (fgets(line, sizeof(line), f) && i < MAX_BOOKMARKS) {
             if (*line == '#') {
                 continue;
             }
-            strcpy(str, strchr(line, '/') + 1);
+            strncpy(str, strchr(line, '/') + 1, PATH_MAX);
             str[strlen(str) - 2] = '\0'; // -1 for newline - 1 for closing Double quotation mark
-            sprintf(bookmarks[i], "%s/%s", home_dir, str);
+            snprintf(bookmarks[i], PATH_MAX, "%s/%s", home_dir, str);
             i++;
         }
         fclose(f);
@@ -73,7 +73,7 @@ void add_file_to_bookmarks(const char *str) {
         fclose(f);
         print_info(bookmark_added, INFO_LINE);
         if (num_bookmarks < MAX_BOOKMARKS) {
-            strcpy(bookmarks[num_bookmarks], str);
+            strncpy(bookmarks[num_bookmarks], str, PATH_MAX);
             num_bookmarks++;
             update_bookmarks_tabs();
         } else {
@@ -137,9 +137,9 @@ void manage_enter_bookmarks(struct stat current_file_stat) {
     char str[PATH_MAX + 1];
     
     if (access(str_ptr[active][ps[active].curr_pos], F_OK ) != -1 ) {
-        strcpy(str, str_ptr[active][ps[active].curr_pos]);
+        strncpy(str, str_ptr[active][ps[active].curr_pos], PATH_MAX);
         if (!S_ISDIR(current_file_stat.st_mode)) {
-            strcpy(ps[active].old_file, strrchr(str_ptr[active][ps[active].curr_pos], '/') + 1);
+            strncpy(ps[active].old_file, strrchr(str_ptr[active][ps[active].curr_pos], '/') + 1, PATH_MAX);
             int len = strlen(str_ptr[active][ps[active].curr_pos]) - strlen(ps[active].old_file);
             str[len] = '\0';
         } else {

commit 10918292f82e1d6a759e5220e8c7d5efa6c12845

fixed a nasty bug introduced in 501881197627e70b68107a13ca852ff1008cf15e Switched from ppoll to poll + signalfd (it seems to me a cleaner solution); Improved some code

inc/declarations.h
@@ -70,7 +70,8 @@
 #define INOTIFY_IX1 2
 #define INOTIFY_IX2 3
 #define INFO_IX 4
-#define DEVMON_IX 5
+#define SIGNAL_IX 5
+#define DEVMON_IX 6
 
 /*
  * Useful macro to know number of elements in arrays
@@ -126,7 +127,7 @@ struct scrstr {
     char tot_size[30];
 };
 
-enum working_mode {normal, fast_browse_, bookmarks_, search_, device_, _preview};
+enum working_mode {normal, fast_browse_, bookmarks_, search_, device_, preview_};
 
 /*
  * Struct used to store tab's information
@@ -178,7 +179,6 @@ typedef struct thread_list {
  * to be printed to main_poll.
  */
 struct pollfd *main_p;
-sigset_t main_mask;
 int nfds, info_fd[2];
 
 thread_job_list *thread_h;

src/archiver.c
@@ -35,7 +35,7 @@ int create_archive(void) {
  */
 static void archiver_func(void) {
     file_list *tmp = thread_h->selected_files;
-    char path[PATH_MAX + 1];
+    char path[PATH_MAX + 1] = {0};
 
     while (tmp) {
         strncpy(path, tmp->name, PATH_MAX);
@@ -48,7 +48,7 @@ static void archiver_func(void) {
 }
 
 static int recursive_archive(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
-    char buff[BUFF_SIZE], entry_name[PATH_MAX + 1];
+    char buff[BUFF_SIZE], entry_name[PATH_MAX + 1] = {0};
     int len, fd;
     struct archive_entry *entry = archive_entry_new();
 
@@ -84,7 +84,7 @@ int extract_file(void) {
 
 static int try_extractor(file_list *tmp) {
     struct archive *a;
-    char *current_dir, path[PATH_MAX + 1];
+    char *current_dir, path[PATH_MAX + 1] = {0};
 
     a = archive_read_new();
     archive_read_support_filter_all(a);
@@ -111,7 +111,7 @@ static void extractor_thread(struct archive *a, const char *current_dir) {
     int len, num;
     int flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
     char buff[BUFF_SIZE], fullpathname[PATH_MAX + 1];
-    char name[PATH_MAX + 1], tmp_name[PATH_MAX + 1];
+    char name[PATH_MAX + 1] = {0}, tmp_name[PATH_MAX + 1] = {0};
 
     ext = archive_write_disk_new();
     archive_write_disk_set_options(ext, flags);

src/bookmarks.c
@@ -37,7 +37,7 @@ static void get_xdg_dirs(void) {
     int i = 0;
     FILE *f;
     char str[PATH_MAX + 1] = {0};
-    char line[1000], file_path[PATH_MAX + 1];
+    char line[1000], file_path[PATH_MAX + 1] = {0};
     const char path[] = "/.config/user-dirs.dirs";
 
     snprintf(file_path, PATH_MAX, "%s%s", home_dir, path);
@@ -65,7 +65,7 @@ void add_file_to_bookmarks(const char *str) {
     char c;
 
     ask_user(_(bookmarks_add_quest), &c, 1);
-    if (quit || c == _(no)[0] || c == 27) {
+    if (c == _(no)[0] || c == 27) {
         return;
     }
     if ((f = fopen(fullpath, "a+"))) {
@@ -91,7 +91,7 @@ void remove_bookmark_from_file(void) {
         print_info(bookmarks_xdg_err, ERR_LINE);
     } else {
         ask_user(_(bookmarks_rm_quest), &c, 1);
-        if (quit || c ==  _(no)[0] || c == 27) {
+        if (c ==  _(no)[0] || c == 27) {
             return;
         }
         remove_bookmark();
@@ -134,7 +134,7 @@ void show_bookmarks(void) {
 
 void manage_enter_bookmarks(struct stat current_file_stat) {
     char c;
-    char str[PATH_MAX + 1];
+    char str[PATH_MAX + 1] = {0};
     
     if (access(str_ptr[active][ps[active].curr_pos], F_OK ) != -1 ) {
         strncpy(str, str_ptr[active][ps[active].curr_pos], PATH_MAX);
@@ -148,7 +148,7 @@ void manage_enter_bookmarks(struct stat current_file_stat) {
         leave_special_mode(str);
     } else {
         ask_user(_(inexistent_bookmark), &c, 1);
-        if (!quit && c != _(no)[0] && c != 27) {
+        if (c != _(no)[0] && c != 27) {
             remove_bookmark();
         }
     }

src/fm_functions.c
@@ -30,7 +30,7 @@ int change_dir(const char *str, int win) {
     
     if (chdir(str) != -1) {
         getcwd(ps[win].my_cwd, PATH_MAX);
-        snprintf(ps[win].title, PATH_MAX, "%s", ps[win].my_cwd);
+        strncpy(ps[win].title, ps[win].my_cwd, PATH_MAX);
         tab_refresh(win);
         inotify_rm_watch(ps[win].inot.fd, ps[win].inot.wd);
         ps[win].inot.wd = inotify_add_watch(ps[win].inot.fd, ps[win].my_cwd, event_mask);
@@ -91,7 +91,7 @@ void manage_file(const char *str) {
         print_info(package_warn, INFO_LINE);
         ask_user(_(pkg_quest), &c, 1);
         print_info("", INFO_LINE);
-        if (!quit && c == _(yes)[0]) {
+        if (c == _(yes)[0]) {
             pthread_create(&install_th, NULL, install_package, (void *)str);
         }
         return;
@@ -153,10 +153,10 @@ static void open_file(const char *str) {
  * Notifies user.
  */
 void fast_file_operations(const int index) {
-    char new_name[NAME_MAX + 1];
+    char new_name[NAME_MAX + 1] = {0};
 
     ask_user(_(ask_name), new_name, NAME_MAX);
-    if (quit || !strlen(new_name) || new_name[0] == 27) {
+    if (!strlen(new_name) || new_name[0] == 27) {
         return;
     }
     int r = short_func[index](new_name);
@@ -229,7 +229,7 @@ void manage_space_press(const char *str) {
  * from where it was copied. If it is the case, it does not copy it.
  */
 int paste_file(void) {
-    char *copied_file_dir, path[PATH_MAX + 1];
+    char *copied_file_dir, path[PATH_MAX + 1] = {0};
 
     for (file_list *tmp = thread_h->selected_files; tmp; tmp = tmp->next) {
         strncpy(path, tmp->name, PATH_MAX);
@@ -248,7 +248,7 @@ int paste_file(void) {
  * Else, the function has to copy it and rm copied file.
  */
 int move_file(void) {
-    char pasted_file[PATH_MAX + 1], *copied_file_dir, path[PATH_MAX + 1];
+    char pasted_file[PATH_MAX + 1] = {0}, *copied_file_dir, path[PATH_MAX + 1] = {0};
     struct stat file_stat_copied, file_stat_pasted;
 
     lstat(thread_h->full_path, &file_stat_pasted);
@@ -282,7 +282,7 @@ int move_file(void) {
  * 2) /path/to/pasted/folder/Scripts/me, that is exactly what we wanted.
  */
 static void cpr(file_list *tmp) {
-    char path[PATH_MAX + 1];
+    char path[PATH_MAX + 1] = {0};
     
     strncpy(path, tmp->name, PATH_MAX);
     distance_from_root = strlen(dirname(path));
@@ -291,7 +291,7 @@ static void cpr(file_list *tmp) {
 
 static int recursive_copy(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
     int len, fd_to, fd_from, ret = 0;
-    char pasted_file[PATH_MAX + 1];
+    char pasted_file[PATH_MAX + 1] = {0};
 
     snprintf(pasted_file, PATH_MAX, "%s%s", thread_h->full_path, path + distance_from_root);
     if (typeflag == FTW_D) {
@@ -362,7 +362,7 @@ static void rmrf(const char *path) {
  */
 void fast_browse(wint_t c) {
     int i = 0;
-    char mbstr[NAME_MAX + 1];
+    char mbstr[NAME_MAX + 1] = {0};
     uint64_t diff = (MILLION * timer.tv_sec) + timer.tv_usec;
 
     gettimeofday(&timer, NULL);
@@ -408,7 +408,7 @@ void save_old_pos(int win) {
     char *str;
     
     str = strrchr(ps[win].nl[ps[win].curr_pos], '/') + 1;
-    strncpy(ps[win].old_file, str, PATH_MAX);
+    strncpy(ps[win].old_file, str, NAME_MAX);
 }
 
 int get_mimetype(const char *path, const char *test) {

src/log.c
@@ -7,7 +7,7 @@ static void log_current_options(void);
 
 void open_log(void) {
     const char log_name[] = "ncursesfm.log";
-    char log_path[PATH_MAX + 1];
+    char log_path[PATH_MAX + 1] = {0};
     
     if (config.loglevel != NO_LOG) {
         snprintf(log_path, PATH_MAX, "%s/.%s", getpwuid(getuid())->pw_dir, log_name);

src/main.c
@@ -28,9 +28,8 @@
 #endif
 
 static void locale_init(void);
-static void set_signals(void);
+static int set_signals(void);
 static void set_pollfd(void);
-static void sig_handler(int signum);
 static void sigsegv_handler(int signum);
 #ifdef LIBX11_PRESENT
 static void check_X(void);
@@ -66,7 +65,6 @@ int previewer_available, previewer_script_available;
 int main(int argc, const char *argv[])
 {
     locale_init();
-    set_signals();
     helper_function(argc, argv);
 #ifdef LIBCONFIG_PRESENT
     check_config_files();
@@ -78,9 +76,7 @@ int main(int argc, const char *argv[])
     set_pollfd();
     if (!quit) {
         screen_init();
-        if (!quit) {
-            main_loop();
-        }
+        main_loop();
     }
     program_quit();
 }
@@ -94,25 +90,27 @@ static void locale_init(void) {
     textdomain("ncursesFM");
 }
 
-static void set_signals(void) {
-    struct sigaction main_act = {{0}};
+static int set_signals(void) {
     sigset_t mask;
-
-    main_act.sa_handler = sig_handler;
-    sigaction(SIGINT, &main_act, 0);
-    sigaction(SIGTERM, &main_act, 0);
+    
+    // when receiving segfault signal,
+    // call our sigsegv handler that just logs
+    // a debug message before dying
+    signal(SIGSEGV, sigsegv_handler);
+    
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
     sigaddset(&mask, SIGTERM);
-    sigprocmask(SIG_BLOCK, &mask, &main_mask);
-    signal(SIGSEGV, sigsegv_handler);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+    
+    return signalfd(-1, &mask, 0);
 }
 
 static void set_pollfd(void) {
 #ifdef SYSTEMD_PRESENT
-    nfds = 6;
+    nfds = 7;
 #else
-    nfds = 5;
+    nfds = 6;
 #endif
     
     main_p = malloc(nfds * sizeof(struct pollfd));
@@ -142,24 +140,16 @@ static void set_pollfd(void) {
         .fd = info_fd[0],
         .events = POLLIN,
     };
+    main_p[SIGNAL_IX] = (struct pollfd) {
+        .fd = set_signals(),
+        .events = POLLIN,
+    };
 #ifdef SYSTEMD_PRESENT
     main_p[DEVMON_IX] = (struct pollfd) {
         .fd = start_monitor(),
         .events = POLLIN,
     };
 #endif
-}
-
-/*
- * if received an external SIGINT or SIGTERM,
- * just switch the quit flag to 1 and log a warn.
- */
-static void sig_handler(int signum) {
-    char str[50];
-
-//     sprintf(str, "received signal %d. Leaving.", signum);
-    WARN(str);
-    quit = NORM_QUIT;
 }
 
 /*
@@ -282,7 +272,7 @@ static void parse_cmd(int argc, const char *argv[]) {
 static void check_config_files() {
     read_config_file(CONFDIR);
     
-    char home_config_path[PATH_MAX + 1];
+    char home_config_path[PATH_MAX + 1] = {0};
     
     snprintf(home_config_path, PATH_MAX, "%s/.config", getpwuid(getuid())->pw_dir);
     read_config_file(home_config_path);
@@ -290,7 +280,7 @@ static void check_config_files() {
 
 static void read_config_file(const char *dir) {
     config_t cfg;
-    char config_file_name[PATH_MAX + 1];
+    char config_file_name[PATH_MAX + 1] = {0};
     const char *str_editor, *str_starting_dir, 
                 *str_borders, *str_cursor, *sysinfo;
 
@@ -411,7 +401,7 @@ static void main_loop(void) {
 #endif
 
     while (!quit) {
-        if (ps[1].mode == _preview && strcmp(old_file, str_ptr[active][ps[active].curr_pos])) {
+        if (ps[1].mode == preview_ && strcmp(old_file, str_ptr[active][ps[active].curr_pos])) {
             preview_img(str_ptr[active][ps[active].curr_pos]);
             strncpy(old_file, str_ptr[active][ps[active].curr_pos], PATH_MAX);
         }
@@ -435,7 +425,7 @@ static void main_loop(void) {
             break;
         case KEY_RIGHT:
         case KEY_LEFT:
-            if (cont == MAX_TABS && ps[1].mode != _preview) {
+            if (cont == MAX_TABS && ps[1].mode != preview_) {
                 change_tab();
             }
             break;
@@ -523,9 +513,9 @@ static void main_loop(void) {
             break;
         case 'j': // j to trigger image previewer
             if (cont == 1 && previewer_available && has_X && previewer_script_available) {
-                ps[1].mode = _preview;
+                ps[1].mode = preview_;
                 add_new_tab();
-            } else if (ps[1].mode == _preview) {
+            } else if (ps[1].mode == preview_) {
                 cont--;
                 delete_tab(1);
                 resize_tab(0, 0);
@@ -616,7 +606,7 @@ static void manage_enter(struct stat current_file_stat) {
 
 static void manage_enter_search(struct stat current_file_stat) {
     char *str = NULL;
-    char path[PATH_MAX + 1];
+    char path[PATH_MAX + 1] = {0};
     
     strncpy(path, sv.found_searched[ps[active].curr_pos], PATH_MAX);
     if (!S_ISDIR(current_file_stat.st_mode)) {
@@ -674,7 +664,7 @@ static int check_init(int index) {
     }
     if (index == EXTRACTOR_TH) {
         ask_user(_(extr_question), &x, 1);
-        if (quit || x == _(no)[0] || x == 27) {
+        if (x == _(no)[0] || x == 27) {
             return 0;
         }
     }
@@ -682,7 +672,7 @@ static int check_init(int index) {
         return check_access();
     }
     ask_user(_(sure), &x, 1);
-    if (quit || x != _(yes)[0]) {
+    if (x != _(yes)[0]) {
         return 0;
     }
     return 1;

src/mount.c
@@ -37,7 +37,7 @@ static int mount_fs(const char *str, int mount) {
     char obj_path[PATH_MAX + 1] = "/org/freedesktop/UDisks2/block_devices/";
     char tmp[30], method[10];
     int r, ret = -1;
-    char mounted_path[PATH_MAX + 1] = {0}, old_cwd[PATH_MAX + 1];
+    char mounted_path[PATH_MAX + 1] = {0}, old_cwd[PATH_MAX + 1] = {0};
     int win;
 
     r = sd_bus_open_system(&mount_bus);
@@ -384,8 +384,8 @@ static int add_callback(sd_bus_message *m, void *userdata, sd_bus_error *ret_err
     int r;
     const char *path;
     const char obj[] = "/org/freedesktop/UDisks2/block_devices/";
-    char devname[PATH_MAX + 1];
-    char name[NAME_MAX + 1];
+    char devname[PATH_MAX + 1] = {0};
+    char name[NAME_MAX + 1] = {0};
     struct udev_device *dev;
 
     r = sd_bus_message_read(m, "o", &path);
@@ -423,8 +423,8 @@ static int remove_callback(sd_bus_message *m, void *userdata, sd_bus_error *ret_
     int r;
     const char *path;
     const char obj[] = "/org/freedesktop/UDisks2/block_devices/";
-    char devname[PATH_MAX + 1];
-    char name[NAME_MAX + 1];
+    char devname[PATH_MAX + 1] = {0};
+    char name[NAME_MAX + 1] = {0};
 
     r = sd_bus_message_read(m, "o", &path);
     if (r < 0) {
@@ -458,7 +458,7 @@ static int change_callback(sd_bus_message *m, void *userdata, sd_bus_error *ret_
     int r;
     const char *path;
     const char obj[] = "org.freedesktop.UDisks2.Filesystem";
-    char devname[PATH_MAX + 1];
+    char devname[PATH_MAX + 1] = {0};
 
     r = sd_bus_message_read(m, "s", &path);
     if (r < 0) {
@@ -628,7 +628,7 @@ void manage_enter_device(void) {
     int pos = ps[active].curr_pos;
     int len = strlen(my_devices[pos]);
     char *ptr = strchr(my_devices[pos], ',');
-    char dev_path[PATH_MAX + 1], name[PATH_MAX + 1] = {0};
+    char dev_path[PATH_MAX + 1] = {0}, name[PATH_MAX + 1] = {0};
 
     mount = my_devices[pos][len - 1] - '0';
     strncpy(dev_path, my_devices[pos], PATH_MAX);

src/print.c
@@ -8,7 +8,7 @@ void print_support(const char *str) {
     char c;
 
     ask_user(_(print_question), &c, 1);
-    if (!quit && c == _(yes)[0]) {
+    if (c == _(yes)[0]) {
         print_file(str);
     }
 }

src/search.c
@@ -6,8 +6,8 @@ static void *search_thread(void *x);
 
 void search(void) {
     ask_user(_(search_insert_name), sv.searched_string, 20);
-    if (quit || strlen(sv.searched_string) < 5 || sv.searched_string[0] == 27) {
-        if (strlen(sv.searched_string) > 0) {
+    if (strlen(sv.searched_string) < 5 || sv.searched_string[0] == 27) {
+        if (strlen(sv.searched_string) > 0 && sv.searched_string[0] != 27) {
             print_info(searched_string_minimum, ERR_LINE);
         }
     } else {
@@ -17,7 +17,7 @@ void search(void) {
         sv.search_archive = 0;
         sv.search_lazy = 0;
         ask_user(_(search_archives), &c, 1);
-        if (quit || c == 27) {
+        if (c == 27) {
             return;
         }
         if (c == _(yes)[0]) {
@@ -30,7 +30,7 @@ void search(void) {
          */
         if (sv.searched_string[0] != '.') {
             ask_user(_(lazy_search), &c, 1);
-            if (quit || c == 27) {
+            if (c == 27) {
                 return;
             }
             if (c == _(yes)[0]) {

src/timer.c
@@ -73,7 +73,7 @@ static void update_battery(int where) {
                     perc[i] = -1;
                 }
                 if (udev_device_get_property_value(dev, "POWER_SUPPLY_NAME")) {
-                    strncpy(name[i], udev_device_get_property_value(dev, "POWER_SUPPLY_NAME"), 9);
+                    strcpy(name[i], udev_device_get_property_value(dev, "POWER_SUPPLY_NAME"));
                 } else {
                     strcpy(name[i], "BAT");
                 }

src/ui_functions.c
@@ -21,6 +21,7 @@ static void show_stat(int init, int end, int win);
 static void erase_stat(void);
 static void info_print(const char *str, int i);
 static void fix_input_cursor_pos(void);
+static void sig_handler(int fd);
 static void info_refresh(int fd);
 static void inotify_refresh(int win);
 static void print_additional_wins(int helper_height, int resizing);
@@ -186,8 +187,7 @@ static int typesort(const struct dirent **d1, const struct dirent **d2) {
  * Clear tab, reset every var, if stat_active was idle, turn it on,
  * then call list_everything.
  */
-void reset_win(int win)
-{
+void reset_win(int win) {
     wclear(ps[win].mywin.fm);
     ps[win].mywin.delta = 0;
     ps[win].curr_pos = 0;
@@ -206,9 +206,9 @@ void reset_win(int win)
  */
 static void list_everything(int win, int old_dim, int end) {
     char *str;
-    wchar_t name[NAME_MAX + 1];
+    wchar_t name[NAME_MAX + 1] = {0};
     
-    if (ps[win].mode == _preview) {
+    if (ps[win].mode == preview_) {
         return;
     }
     
@@ -297,7 +297,7 @@ void new_tab(int win) {
     idlok(ps[win].mywin.fm, TRUE);
     notimeout(ps[win].mywin.fm, TRUE);
     nodelay(ps[win].mywin.fm, TRUE);
-    if (ps[win].mode != _preview) {
+    if (ps[win].mode != preview_) {
         initialize_tab_cwd(win);
     }
 }
@@ -820,7 +820,11 @@ void ask_user(const char *str, char *input, int d) {
             break;
         }
     } while ((wcslen(wstring) < d) && (!quit) && (!leave));
-    if (wcslen(wstring) > 0 && !quit) {
+    if (quit) {
+        wmemset(wstring, 0,  wcslen(wstring));
+        input[0] = 27;
+    }
+    if (wcslen(wstring) > 0) {
         wcstombs(input, wstring, d);
     }
     curs_set(0);
@@ -838,14 +842,8 @@ static void fix_input_cursor_pos(void) {
 }
 
 /*
- * call ppoll; it is interruptable from SIGINT and SIGTERM signals.
- * It will poll for getch, timerfd, inotify, pipe and bus events.
- * If occurred event is not a getch event, return recursively main_poll
- * (ie: we don't need to come back to the cycle that called main_poll, because no buttons has been pressed)
- * else return getch value (it will be ERR if a signal has been received, as ppoll gets interrupted but
- * getch is in notimeout mode)
- * If ppoll return -1 it means: window has been resized or we received a signal (sigint/sigterm).
- * Either case, just return wgetch.
+ * call poll; it is interruptable from SIGINT and SIGTERM signals (signalfd)
+ * It will poll for getch, timerfd, inotify, pipe, signalfd and bus events.
  */
 wint_t main_poll(WINDOW *win) {;
     uint64_t t;
@@ -858,13 +856,13 @@ wint_t main_poll(WINDOW *win) {;
     while ((ret == ERR) && (!quit)) {
         fix_input_cursor_pos(); // if we're currently asking a question, move cursor to its correct position on ASK_LINE
         /*
-        * resize event returns -EPERM error with ppoll (-1)
+        * resize event returns -EPERM error with poll (-1)
         * see here: http://keyvanfatehi.com/2011/08/02/Asynchronous-c-programs-an-event-loop-and-ncurses/.
         * plus, this is needed when a signal is caught (sigint/sigterm)
         */
-        int r = ppoll(main_p, nfds, NULL, &main_mask);
+        int r = poll(main_p, nfds, -1);
         if (r == -1) {
-            ret = wget_wch(win, &c);
+            ret = wget_wch(win, &c); // ungetch(KEY_RESIZE);
         } else {
             for (int i = 0; i < nfds && r > 0; i++) {
                 if (main_p[i].revents & POLLIN) {
@@ -887,6 +885,10 @@ wint_t main_poll(WINDOW *win) {;
                     /* we received an event from pipe to print an info msg */
                         info_refresh(main_p[i].fd);
                         break;
+                    case SIGNAL_IX:
+                    /* we received a signal */
+                        sig_handler(main_p[i].fd);
+                        break;
 #ifdef SYSTEMD_PRESENT
                     case DEVMON_IX:
                     /* we received a bus event */
@@ -900,6 +902,25 @@ wint_t main_poll(WINDOW *win) {;
         }
     }
     return c;
+}
+
+/*
+ * if received an external SIGINT or SIGTERM,
+ * just switch the quit flag to 1 and log a warn.
+ */
+static void sig_handler(int fd) {
+    char str[50];
+    struct signalfd_siginfo fdsi;
+    ssize_t s;
+    
+    s = read(fd, &fdsi, sizeof(struct signalfd_siginfo));
+    if (s != sizeof(struct signalfd_siginfo)) {
+        ERROR("an error occurred while getting signalfd data.");
+    } else {
+        sprintf(str, "received signal %d. Leaving.", fdsi.ssi_signo);
+        WARN(str);
+    }
+    quit = NORM_QUIT;
 }
 
 void timer_event(void) {
@@ -1250,7 +1271,7 @@ void preview_img(const char *path) {
     int cmd;
     char full_cmd[PATH_MAX + 1];
     
-    if (ps[1].mode == _preview) {
+    if (ps[1].mode == preview_) {
         if (get_mimetype(path, "image")) {
             // 1 to print image
             cmd = 1;

src/worker_thread.c
@@ -52,7 +52,7 @@ void init_thread(int type, int (* const f)(void)) {
     if (!(thread_h = add_job(thread_h, type, f))) {
         return;
     }
-    if (init_thread_helper()) {
+    if (init_thread_helper() == -1) {
         return;
     }
     if (num_of_jobs > 1) {
@@ -75,12 +75,12 @@ void init_thread(int type, int (* const f)(void)) {
  * Fixes some needed current_th variables.
  */
 static int init_thread_helper(void) {
-    char name[NAME_MAX + 1];
+    char name[NAME_MAX + 1] = {0};
     int num = 1, len;
 
-    if (current_th->type == ARCHIVER_TH && !quit) {
+    if (current_th->type == ARCHIVER_TH) {
         ask_user(_(archiving_mesg), name, NAME_MAX);
-        if (quit || name[0] == 27) {
+        if (name[0] == 27) {
             free(current_th);
             current_th = NULL;
             return -1;